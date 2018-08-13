42494ee0536de98650e3ea466d97fb6320179f00
Kaan <Kaan@Kaans-Mac.local>
2018-08-13 07:22:24 -0700

    first update

---
+++ index.js
@@ -0,0 +1,27 @@
+//discribe the game rules. for example; console.log("to start, enter "node letter.js start" + "\n\n you have to guess the leeters of the displayed word
+// if you run out of guess #s, then you loose, otherwise you win. Lets type "node letter.js start" ")
+console.log("to start, enter node letter.js start" + "\n\n you have to guess the leeters of the displayed word if you run out of guess #s, then you loose, otherwise you win. Lets type 'node letter.js start' "); 
+
+//install modules here
+
+var inquirer = require("inquirer"); 
+var fs = require("fs"); 
+var letter = require("./letter.js"); 
+
+
+
+
+
+//get CLI commends here, use "process.argv.slice(3).join(" ")" if needed.
+
+
+
+
+
+// create a constructor that calls a word from wordbank at word.js 
+   //create a function game() 
+   this.game = function(){
+
+
+}
+
---
+++ word.js
@@ -0,0 +1,16 @@
+// contructor will contain letter bank
+
+var WordBank = function(){
+
+    var words = ["string", "taste", "shelf", "fool", "excite", "rail", "release", "enjoy", "trade", "dorks", "girl", "sticks",
+    "pretend", "relieved", "pickle", "volleyball", "plate", "puncture", "panoramic", "name", "motionless", "wail", "sad", "knowledge"]; 
+    
+
+}
+
+
+
+
+
+module.exports = WordBank; 
+
