b0705160ac8fcc5b2fe7c91b1bfc259fc4762bd3
Kaan <Kaan@Kaans-Mac.local>
2018-08-13 13:34:07 -0700

    word.js added

--- word.js
+++ word.js
@@ -1,20 +1,27 @@
 // contructor will contain letter bank
-var letter = require("./letter"); 
+var letter = require("./letter");
 
-var WordBank = function(){
+var WordBank = function () {
 
-    var wordBank = ["string", "taste", "shelf", "fool", "excite", "rail", "release", "enjoy", "trade", "dorks", "girl", "sticks",
-    "pretend", "relieved", "pickle", "volleyball", "plate", "puncture", "panoramic", "name", "motionless", "wail", "sad", "knowledge"]; 
-    
-    var randomNumber = Math.floor(Math.random()*wordBank.length); 
+    this.wordBank = ["string", "taste", "shelf", "fool", "excite", "rail", "release", "enjoy", "trade", "dorks", "girl", "sticks",
+        "pretend", "relieved", "pickle", "volleyball", "plate", "puncture", "panoramic", "name", "motionless", "wail", "sad", "knowledge"];
+
+    this.randomNumber = Math.floor(Math.random() * wordBank.length);
+
 
     //create a for look that checks letter.IswordResquested === true and sends a word
+    if (letter.IsWordRequested) {
+        // for (var i = 0; i < 1; i++){
+        letter.LetterFunction(this.wordBank[this.randomNumber]);
+        letter.IsWordRequested == false;
+        // }
 
- }
+    }
+}
 
 
 
 
 
-module.exports = WordBank; 
+module.exports = WordBank;
 
